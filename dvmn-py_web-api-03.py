2944eaf3d6abaa6cb271dce56676b86daaf34fb3
kurtsev-artem <66749796+kurtsev-artem@users.noreply.github.com>
2025-12-20 22:01:10 +0300

    Add files via upload

--- dvmn-py_web-api-03.py
+++ dvmn-py_web-api-03.py
@@ -48,18 +48,14 @@ def main():
     url = namespace.url
     parsed_url = urlparse(url)
 
-    if  is_shorten_link(token, parsed_url.path[1:]):
-        try:
+    try:
+        if  is_shorten_link(token, parsed_url.path[1:]):
             print('Количество кликов: {}'.format(count_clicks(token, parsed_url.path[1:])))
-        except requests.exceptions.HTTPError:
-            print('Вы ввели неправильную ссылку или неверный токен.')
-            raise
-    else:  
-        try:
+        else:  
             print('Сокращенная ссылка: {}'.format(shorten_link(token, url)))
-        except requests.exceptions.HTTPError:
-            print('Вы ввели неправильную ссылку или неверный токен.')
-            raise
+    except requests.exceptions.HTTPError:
+        print('Вы ввели неправильную ссылку или неверный токен.')
+        raise
 
 
 if __name__ == '__main__':
